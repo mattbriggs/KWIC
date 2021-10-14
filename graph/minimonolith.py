3b92eacfab0843fa1a212ed173051adbaae0dbef
Matt Briggs <mabrigg@microsoft.com>
2021-10-14 16:12:40 -0700

    final day of hackathon

--- graph/minimonolith.py
+++ graph/minimonolith.py
@@ -7,6 +7,7 @@ This is a sketch module of the basic functionality to creat a keyword in context
 
 import os
 import json
+import uuid
 from git import Repo
 from neo4j import GraphDatabase
 import mdparser as MDP
@@ -29,6 +30,12 @@ def create_cipher(in_dict):
     roll_cipher = roll_cipher.replace("\\", "\\\\")
     return roll_cipher
 
+def clear_line(instring):
+    '''Clear problem characters'''
+    instring = instring.replace("\\", "\\\\")
+    instring = instring.replace("\"", "\\\"")
+    instring = instring.replace("\'", "\\\'")
+    return instring
 
 def get_text_from_file(path):
     '''Return text from a text filename path'''
@@ -63,14 +70,18 @@ def create_node(tx, type, map):
     tx.run('CREATE (n: {}) SET n = {}'.format(type, map))
 
 
-def create_edge(tx, type1, node1, edge, type2, node2):
-    session.write_transaction(create_node, type1, node1)
-    session.write_transaction(create_node, type2, node2)
-    cypher = '''MATCH (a:{} {{name: "node1"}})
-MATCH (b:{} {{name: "node2"}})
+def create_edge(tx, source, edge, target):
+    '''Send the source node to the target node:
+    a:type {name: "unique1"), b:type {name: "unique2"), (a) -[r:EDGE]-> (b)
+    Each node:
+    node type
+    ID attribitue (such as name)
+    Unique name.
+    '''
+    cypher = '''MATCH (a:{} {{{}: "{}"}})
+MATCH (b:{} {{{}: "{}"}})
 MERGE (a)-[r:{}]->(b)
-    '''.format(type1, type2, edge)
-    print(cypher)
+    '''.format(source[0], source[1], source[2], target[0], target[1], target[2], edge)
     tx.run(cypher)
 
 # logic
@@ -90,6 +101,7 @@ map = "{ name: '"+config["docset"]+"'}"
 with driver.session() as session:
     session.write_transaction(create_node, "docset", map)
 
+
 for f in list_of_files:
     try:
         mdparser = MDP.MDParser()
@@ -102,21 +114,31 @@ for f in list_of_files:
 
         with driver.session() as session:
             session.write_transaction(create_node, "article", map)
-        # with driver.session() as session:
-        #     session.write_transaction(create_edge, "docset", "azure-stack-docs", "CONTAINS", "article", path)
+        
+        source = ["article", "path", path]
+        target = ["docset", "name", config["docset"]] 
+        with driver.session() as session:
+            session.write_transaction(create_edge, source, "CONTAINS", target)
+        
+
+        for indx, i in enumerate(text_as_lines):
+            line_text = clear_line(i)
+            line_no = indx+1
+            line_id = str(uuid.uuid4())
+            line_map = "{ lineid : '" + line_id + "', "
+            line_map += "lineno: '" + str(line_no) + "', "
+            line_map += "text: '" + line_text + "' }"
+            print(line_map)
+            with driver.session() as session:
+                session.write_transaction(create_node, "line", line_map)
+            driver.close()
+            source_line = ["line", "lineid", line_id]
+            target_doc = ["article", "path", path]
+            with driver.session() as session:
+                session.write_transaction(create_edge, source_line, "BODY", target_doc)
+            
+
     except:
-        print("Error: {}".format(f))
-
-
-# for indx, i in enumerate(text_as_lines):
-#     line_no = indx+1
-#     print("{} : {}".format(line_no, i))
-
-
-
-# map = "{ name: 'doc-set', item: 'test item 2' }"
-
-# with driver.session() as session:
-#     session.write_transaction(create_node, map)
+        print("*** Error: {}".format(f))
 
 driver.close()
--- graph/singlefile.py
+++ graph/singlefile.py
@@ -1,4 +1,12 @@
+import uuid
 import mdparser as MDP
+
+def clear_line(instring):
+    '''Clear problem characters'''
+    instring = instring.replace("\\", "\\\\")
+    instring = instring.replace("\"", "\\\"")
+    instring = instring.replace("\'", "\\\'")
+    return instring
 
 infile = r"C:\git\mb\KWIC\graph\clone\azure-stack\user\azure-stack-compute-overview.md"
 
@@ -10,5 +18,13 @@ metadata = mdparser.process_meta()
 print(metadata)
 
 for indx, i in enumerate(text_as_lines):
+    line_text = clear_line(i)
     line_no = indx+1
-    print("{} : {}".format(line_no, i))
+    line_id = str(uuid.uuid4())
+    print(line_text)
+    print(line_no)
+    print(line_id)
+    line_map = "{ lineid : '" + line_id + "', "
+    line_map += "lineno: '" + str(line_no) + "', "
+    line_map += "text: '" + line_text + "' }"
+    print(line_map)
